856e6a706f81940750f5d0b02e32e4b163de4aa9
Carl-Magnus <carl@live.no>
2020-08-20 23:54:49 +0200

    Ads Footer, styling for header, and text font

---
+++ my-app/src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.css';
+
+
+function Footer() {
+    //TODO: Remake navbar
+    return(
+        <footer className="footer">
+            <ul className="footerList">
+                <li>
+                <p>
+                    Laget av Carl-Magnus Lein
+                </p>
+                </li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ my-app/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+
+function Header() {
+    //TODO: Remake navbar
+    return(
+        <header className="header">
+            <div className="logo">
+                Rom på Ole Johan Dahls Hus
+            </div>
+        </header>
+    )
+}
+
+export default Header;
