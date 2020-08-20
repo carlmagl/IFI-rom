241545b14d0dd26bb0e7c1c49bdf73fe713274d7
Carl-Magnus <carl@live.no>
2020-08-20 23:54:49 +0200

    Ads Footer, styling for header, and text font

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header.js';
+import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        <div className="Content"/>
+      <Footer/>
     </div>
   );
 }
