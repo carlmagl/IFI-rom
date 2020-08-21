c3faeed1db4b8937033ed9ea147e06754906d669
Carl-Magnus <carl@live.no>
2020-08-21 10:51:23 +0200

    Adds Rooms component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header.js';
 import Footer from './components/Footer/Footer.js';
+import Rooms from './components/Rooms/Rooms.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Rooms/>
         <div className="Content"/>
       <Footer/>
     </div>
