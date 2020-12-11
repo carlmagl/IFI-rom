cb3f14491c33b55487ed69df98d45d2f6d1292d3
Carl-Magnus Lein <cale3010@gmail.com>
2020-12-11 18:22:54 +0100

    Almost all rooms on 3 floor now active

--- src/components/Rooms/Rooms.js
+++ src/components/Rooms/Rooms.js
@@ -10,128 +10,6 @@ const Rooms = (props) => {
     contentType: "rooms",
   });
 
-  /* {
-      name: "Scheme",
-      floor: "1",
-      roomnumber: "1251",
-    },
-    {
-      name: "Smalltalk",
-      floor: "1",
-      roomnumber: "1416",
-    },
-    {
-      name: "Simula",
-      floor: "1",
-      roomnumber: "1423",
-      type: "Forelesningssal",
-    },
-    {
-      name: "Sed",
-      floor: "1",
-      roomnumber: "1454",
-    },
-    {
-      name: "Shell",
-      floor: "1",
-      roomnumber: "1456",
-    },
-    {
-      name: "Python",
-      floor: "2",
-      roomnumber: "2269",
-    },
-    {
-      name: "Java",
-      floor: "2",
-      roomnumber: "2423",
-    },
-    {
-      name: "Lisp",
-      floor: "2",
-      roomnumber: "2428",
-    },
-    {
-      name: "Logo",
-      floor: "2",
-      roomnumber: "2438",
-    },
-    {
-      name: "Modula",
-      floor: "2",
-      roomnumber: "2443",
-    },
-    {
-      name: "Pascal",
-      floor: "2",
-      roomnumber: "2452",
-    },
-    {
-      name: "Perl",
-      floor: "2",
-      roomnumber: "2453",
-    },
-    {
-      name: "Postscript",
-      floor: "2",
-      roomnumber: "2458",
-    },
-    {
-      name: "Prolog",
-      floor: "2",
-      roomnumber: "2465",
-    },
-    {
-      name: "Awk",
-      floor: "3",
-      roomnumber: "3118",
-    },
-    {
-      name: "Ada",
-      floor: "3",
-      roomnumber: "3407",
-    },
-    {
-      name: "Assembler",
-      floor: "3",
-      roomnumber: "3417",
-    },
-    {
-      name: "Limbo",
-      floor: "3",
-      roomnumber: "3418",
-    },
-    {
-      name: "C",
-      floor: "3",
-      roomnumber: "3437",
-    },
-    {
-      name: "Caml",
-      floor: "3",
-      roomnumber: "3438",
-    },
-    {
-      name: "Chill",
-      floor: "3",
-      roomnumber: "3443",
-    },
-    {
-      name: "Cobol",
-      floor: "3",
-      roomnumber: "3452",
-    },
-    {
-      name: "Euler",
-      floor: "3",
-      roomnumber: "3459",
-    },
-    {
-      name: "Fortress",
-      floor: "3",
-      roomnumber: "3468",
-    },
-  ]; */
   if (loading || !fetched) {
     return null;
   }
@@ -158,7 +36,7 @@ const Rooms = (props) => {
         <input
           type="text"
           className="inputField"
-          placeholder="Søk på navn, etasje eller nummer"
+          placeholder="Søk på navn, etasje eller rom-nummer"
           onChange={(e) => {
             setInput(e.target.value);
           }}
