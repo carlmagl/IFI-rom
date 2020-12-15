6b29851c7b45b453a1bddc72a7726f0ee15d9e24
Carl-Magnus Lein <cale3010@gmail.com>
2020-12-15 17:37:42 +0100

    Adds mobile view for svg

--- src/components/Floor/Floor.js
+++ src/components/Floor/Floor.js
@@ -5,6 +5,9 @@ import Button from "@material-ui/core/Button";
 import { ReactComponent as Floor1 } from "./floor1.svg";
 import { ReactComponent as Floor2 } from "./floor2.svg";
 import { ReactComponent as Floor3 } from "./floor3.svg";
+import { ReactComponent as Floor1Mobile } from "./floor1-mobile.svg";
+import { ReactComponent as Floor2Mobile } from "./floor2-mobile.svg";
+import { ReactComponent as Floor3Mobile } from "./floor3-mobile.svg";
 import { useParams, useLocation, useHistory } from "react-router-dom";
 
 function Floor() {
@@ -15,6 +18,7 @@ function Floor() {
   const history = useHistory();
 
   useEffect(() => {
+    console.log(window.innerWidth);
     if (roomnumber != null) {
       var element = document.getElementById(roomnumber);
       element.scrollIntoView({
@@ -40,11 +44,37 @@ function Floor() {
           Tilbake
         </Button>
       </div>
-      <section className="main">
-        {parseInt(floor) === 1 && <Floor1 height={window.innerHeight * 0.7} />}
-        {parseInt(floor) === 2 && <Floor2 height={window.innerHeight * 0.7} />}
-        {parseInt(floor) === 3 && <Floor3 height={window.innerHeight * 0.7} />}
-      </section>
+      {window.innerWidth >= 600 && (
+        <section className="main">
+          {parseInt(floor) === 1 && (
+            <Floor1 height={window.innerHeight * 0.7} />
+          )}
+
+          {parseInt(floor) === 2 && (
+            <Floor2 height={window.innerHeight * 0.7} />
+          )}
+
+          {parseInt(floor) === 3 && (
+            <Floor3 height={window.innerHeight * 0.7} />
+          )}
+        </section>
+      )}
+
+      {window.innerWidth < 600 && (
+        <section className="main-mobile">
+          {parseInt(floor) === 1 && (
+            <Floor1Mobile width={window.innerWidth * 0.9} />
+          )}
+
+          {parseInt(floor) === 2 && (
+            <Floor2Mobile width={window.innerWidth * 0.9} />
+          )}
+
+          {parseInt(floor) === 3 && (
+            <Floor3Mobile width={window.innerWidth * 0.9} />
+          )}
+        </section>
+      )}
     </>
   );
 }
