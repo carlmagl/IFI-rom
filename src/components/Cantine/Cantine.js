cd87bb9a2435825133572d921080b4b85fdc118d
Carl-Magnus Lein <carl@live.no>
2021-10-25 10:16:29 +0200

    Test

--- src/components/Cantine/Cantine.js
+++ src/components/Cantine/Cantine.js
@@ -7,7 +7,7 @@ export function Cantine() {
     useEffect(() => {
         const config = {
           headers: {
-            "content-type": "*",
+            "Content-Type": "text/plain",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
             "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
