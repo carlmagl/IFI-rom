142a64168e5c1a5e455f3ab6f8475c305609f4ca
carlmagl <carl@live.no>
2023-05-10 14:46:48 +0200

    Initialized analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <>
+        <Component {...pageProps} />
+        <Analytics />
+      </>
     </Layout>
   );
 }
